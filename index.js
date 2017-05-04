40b7b7f74d653738fab00a7992d4e8aa4a24fd44
guojia <36590@qq.com>
2017-05-04 16:06:18 +0800

    GuoJia

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 "use strict";
 
-const crypto = require('crypto');
-const xml2js = require('xml2js');
+const Crypto = require("crypto");
+const Xml2js = require("xml2js");
 const KOCReturn = require("koc-common-return");
 
-const StringUtils = {
-  /********************************
-   * IsEmailFormat
-   ********************************/
+const KOCString = {
+  // region IsEmailFormat
   IsEmailFormat: (val) => {
     return (/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/).test(StringUtils.ToString(val));
   },
-  /********************************
-   * IsPhone
-   ********************************/
+  // endregion
+  // region IsPhone
   IsPhone: (val) => {
     return (/^1\d{10}$/).test(StringUtils.ToString(val));
   },
-  /********************************
-   * getStringLength 取得字符串长度(一个中文为两个长度)
-   ********************************/
+  // endregion
+  // region StringLength 取得字符串长度(一个中文为两个长度)
   StringLength: (val) => {
     val = StringUtils.ToString(str);
     const cArr = val.match(/[^\x00-\xff]/ig);
     return val.length + (cArr ? 0 : cArr.length);
   },
+  // endregion
+  // region ToString
   ToString: (val) => {
     return val == undefined ? "" : val.toString();
   },
+  /********************************
+   * ToInt
+   * val
+   * defaultval       默认值(不传为0)
+   * positive         是否必须为正值true/false(当true时如果为负值返回defaultval)
+   ********************************/
+  // endregion
+  // region ToInt
   /********************************
    * ToInt
    * val
@@ -46,12 +52,13 @@ const StringUtils = {
     }
     return val;
   },
-  /********************************
-   * ToIntPositive 返回正值Int
-   ********************************/
+  // endregion
+  // region ToIntPositive
   ToIntPositive: (val, defaultval) => {
     return StringUtils.ToInt(val, defaultval, true);
   },
+  // endregion
+  // region ToFloat
   /********************************
    * ToFloat
    * val
@@ -75,27 +82,23 @@ const StringUtils = {
     return val;
 
   },
-  /********************************
-   * ToFloatPositive 返回正值Float
-   ********************************/
+  // endregion
+  // region ToFloatPositive 返回正值Float
   ToFloatPositive: (val, defaultval, fixed) => {
     return StringUtils.ToFloat(val, defaultval, true, fixed);
   },
-  /********************************
-   * ToCurrency 返回金额(2位小数，string)
-   ********************************/
+  // endregion
+  // region ToCurrency 返回金额(2位小数，string)
   ToCurrency: (val, defaultval, positive) => {
     return StringUtils.ToFloat(val, defaultval, positive, 2);
   },
-  /********************************
-   * ToCurrencyPositive 返回正值金额(2位小数，string)
-   ********************************/
+  // endregion
+  // region ToCurrencyPositive 返回正值金额(2位小数，string)
   ToCurrencyPositive: (val, defaultval) => {
     return StringUtils.ToFloat(val, defaultval, true, 2);
   },
-  /********************************
-   * ToBoolean 返回bool
-   ********************************/
+  // endregion
+  // region ToBoolean 返回bool
   ToBoolean: (val, defaultval) => {
     switch (StringUtils.ToString(val).toLowerCase()) {
       case "true":
@@ -105,50 +108,30 @@ const StringUtils = {
         return !!defaultval;
     }
   },
-  /********************************
-   * MD5
-   ********************************/
+  // endregion
+  // region MD5
   MD5: (val) => {
-    const md5 = crypto.createHash('md5');
+    const md5 = Crypto.createHash('md5');
     md5.update(val, 'utf8');
     return md5.digest('hex').toLowerCase();
   },
-  /********************************
-   * SHA1
-   ********************************/
+  // endregion
+  // region SHA1
   SHA1: (val) => {
-    const shasum = crypto.createHash('sha1');
+    const shasum = Crypto.createHash('sha1');
     shasum.update(val);
     return shasum.digest('hex').toLowerCase();
   },
-  /********************************
-   * RSASHA1
-   ********************************/
+  // endregion
+  // region RSASHA1
   RSASHA1: (val, key) => {
-    const signer = crypto.createSign('RSA-SHA1');
+    const signer = Crypto.createSign('RSA-SHA1');
     signer.update(val, 'utf-8');
     return signer.sign(key, 'base64');
   },
-  //region xml转换成json
-  ParseXML: (xml) => {
-    return new Promise((resolve) => {
-      xml2js.parseString(xml, {trim: true}, function (err, obj) {
-        const retValue = KOCReturn.Value();
-        if (err) {
-          retValue.hasError = true;
-          retValue.message = err.message;
-          retValue.returnObject = err;
-          resolve(retValue);
-          return;
-        }
-        retValue.returnObject = obj;
-        resolve(retValue);
-      });
-    });
-  },
-  //endregion
-  //region 将xml2js解析出来的对象转换成直接可访问的对象
-  FormatXMLJSObj: (result) => {
+  // endregion
+  // region XMLObj2JSON
+  XMLObj2JSON: (result) => {
     let message = {};
     if (typeof result === 'object') {
       for (let key in result) {
@@ -158,20 +141,38 @@ const StringUtils = {
         if (result[key].length === 1) {
           let val = result[key][0];
           if (typeof val === 'object') {
-            message[key] = StringUtils.FormatXMLJSObj(val);
+            message[key] = KOCString.XMLObj2JSON(val);
           } else {
             message[key] = (val || '').trim();
           }
         } else {
           message[key] = result[key].map(function (item) {
-            return StringUtils.FormatXMLJSObj(item);
+            return KOCString.XMLObj2JSON(item);
           });
         }
       }
     }
     return message;
+  },
+  // endregion
+  // region XML2JSON
+  XML2JSON: (val) => {
+    return new Promise((resolve) => {
+      Xml2js.parseString(val, {trim: true}, function (err, obj) {
+        const retValue = KOCReturn.Value();
+        if (err) {
+          retValue.hasError = true;
+          retValue.message = err.message;
+          retValue.returnObject = err;
+          resolve(retValue);
+          return;
+        }
+        retValue.returnObject = KOCString.XMLObj2JSON(obj);
+        resolve(retValue);
+      });
+    });
   }
-  //endregion
+  // endregion
 };
 
-module.exports = StringUtils;
+module.exports = KOCString;
