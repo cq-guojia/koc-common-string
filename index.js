4d295635820d332f67e26a8010e23697f99d10b6
guojia <36590@qq.com>
2017-06-12 16:10:11 +0800

    GuoJia

--- index.js
+++ index.js
@@ -24,12 +24,43 @@ const KOCString = {
   ToString: (val) => {
     return val == undefined ? "" : val.toString();
   },
+  // endregion
+  // region Cover
   /********************************
-   * ToInt
-   * val
-   * defaultval       默认值(不传为0)
-   * positive         是否必须为正值true/false(当true时如果为负值返回defaultval)
+   * Cover
+   * str
+   * type             Phone/IDCard/Email/QQ
+   * char             替代串，默认*
    ********************************/
+  Cover: function (str, type, char) {
+    str = this.ToString(str);
+    if (!str) {
+      return "";
+    }
+    char = char || "*";
+    if (type == "Name") {
+      return str.substr(0, 1) + char + char;
+    } else if ((!type || type == "Phone") && RegularUtils.Verify(str, "Phone")) {
+      return str.substr(0, 3) + char + char + char + char + str.substr(str.length - 4);
+    } else if ((!type || type == "IDCard") && RegularUtils.Verify(str, "IDCard")) {
+      return str.substr(0, 6) + char + char + char + char + char + char + char + char + str.substr(str.length - 4);
+    } else if ((!type || type == "Email") && RegularUtils.Verify(str, "Email")) {
+      var _index = str.indexOf("@");
+      if (_index < 3) {
+        return char + char + char + str.substr(_index);
+      }
+      var _len = this.ToInt(_index / 3);
+      return str.substr(0, _len) + char + char + char + char + char + str.substr(_index - _len);
+    } else if ((!type || type == "QQ") && RegularUtils.Verify(str, "QQ")) {
+      var _len = this.ToInt(str.length / 3);
+      return str.substr(0, _len) + char + char + char + char + char + str.substr(str.length - _len);
+    } else if (str.length >= 3) {
+      var _len = this.ToInt(str.length / 3);
+      return str.substr(0, _len) + char + char + char + char + char + str.substr(str.length - _len);
+    } else {
+      return char + char + char;
+    }
+  },
   // endregion
   // region ToInt
   /********************************
@@ -61,39 +92,62 @@ const KOCString = {
    * ToFloat
    * val
    * defaultval       默认值(不传为0)
-   * positive         是否必须为正值true/false(当true时如果为负值返回defaultval)
-   * fixed            小数位数(四舍五入，返回string值)
+   * fixed            小数位数
+   * str              是否返回字符串
+   * notnegative      是否必须为正值true/false(当true时如果为负值返回defaultval)
    ********************************/
-  ToFloat: (val, defaultval, positive, fixed) => {
-    defaultval = parseFloat(defaultval);
-    defaultval = isNaN(defaultval) ? 0 : defaultval;
+  ToFloat: (val, defaultval, fixed, str, positive, notnegative) => {
+    defaultval = isNaN(parseFloat(defaultval)) ? 0 : parseFloat(defaultval);
     val = parseFloat(val);
     if (isNaN(val)) {
       val = defaultval;
     }
-    if (positive && val < 0) {
-      val = defaultval;
+    if (notnegative && val < 0) {
+      val = defaultval < 0 ? 0 : defaultval;
     }
-    if (fixed) {
-      val = val.toFixed(KOCString.ToIntPositive(fixed));
+    fixed = this.ToInt(fixed, -1);
+    if (fixed >= 0) {
+      val = val.toFixed(fixed);
+    }
+    if (!str) {
+      val = parseFloat(val);
     }
     return val;
-
   },
   // endregion
-  // region ToFloatPositive 返回正值Float
-  ToFloatPositive: (val, defaultval, fixed) => {
-    return KOCString.ToFloat(val, defaultval, true, fixed);
+  // region ToFloatPositive 正数Float
+  ToFloatPositive: function (val, defaultval, fixed) {
+    return this.ToFloat(val, defaultval, fixed, false, true);
   },
   // endregion
-  // region ToCurrency 返回金额(2位小数，string)
-  ToCurrency: (val, defaultval, positive) => {
-    return KOCString.ToFloat(val, defaultval, positive, 2);
+  // region ToFloatStr 字符串Float
+  ToFloatStr: function (val, defaultval, fixed) {
+    return this.ToFloat(val, defaultval, fixed, true);
   },
   // endregion
-  // region ToCurrencyPositive 返回正值金额(2位小数，string)
-  ToCurrencyPositive: (val, defaultval) => {
-    return KOCString.ToFloat(val, defaultval, true, 2);
+  // region ToFloatPositiveStr 正数字符串Float
+  ToFloatPositiveStr: function (val, defaultval, fixed) {
+    return this.ToFloat(val, defaultval, fixed, true, true);
+  },
+  // endregion
+  // region ToCurrency 金额
+  ToCurrency: function (val, defaultval) {
+    return this.ToFloat(val, defaultval, 2);
+  },
+  // endregion
+  // region ToCurrencyPositive 正数金额
+  ToCurrencyPositive: function (val, defaultval) {
+    return this.ToFloat(val, defaultval, 2, false, true);
+  },
+  // endregion
+  // region ToCurrencyStr 字符串金额
+  ToCurrencyStr: function (val, defaultval) {
+    return this.ToFloat(val, defaultval, 2, true);
+  },
+  // endregion
+  // region ToCurrencyPositiveStr 正数字符串金额
+  ToCurrencyPositiveStr: function (val, defaultval) {
+    return this.ToFloat(val, defaultval, 2, true, true);
   },
   // endregion
   // region ToBoolean 返回bool
